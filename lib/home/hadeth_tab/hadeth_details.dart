1d1a23008bfa91a5755a41bd8ca79c8a14eaee41
dalia <doda.mageddd>
2024-02-15 07:46:43 +0200

    finished hadethtab

---
+++ lib/home/hadeth_tab/hadeth_details.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/home/hadeth_tab/hadeth_detail_item.dart';
+import 'package:islami/home/hadeth_tab/hadeth_tab.dart';
+import 'package:islami/home/quran_tab/sura_detail_item.dart';
+import 'package:islami/islami_theme.dart';
+
+class HadethDetails extends StatefulWidget {
+  static String hadeth_details = "hadeth_details";
+
+  @override
+  State<HadethDetails> createState() => _HadethDetailsState();
+}
+
+class _HadethDetailsState extends State<HadethDetails> {
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)!.settings.arguments as Hadeth;
+
+    return Stack(children: [
+      Image.asset(
+        "assets/imgs/background.png",
+        width: double.infinity,
+        height: double.infinity,
+        fit: BoxFit.fill,
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text("${args.title}"),
+        ),
+        body: ListView.builder(itemBuilder: (context,int){
+              return HadethDetailItem(hadethLine:" ${args.content[int]}");
+            },
+          itemCount: args.content.length,
+            ),
+    ),
+
+    ]);
+  }
+}
